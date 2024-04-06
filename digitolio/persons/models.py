361cff795dcc182064ada089f92eed9e49fdaf50
ksiim <makskrutchev@gmail.com>
2024-04-06 16:19:03 +0700

    added person model

--- digitolio/persons/models.py
+++ digitolio/persons/models.py
@@ -1,8 +1,23 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
 class Person(AbstractUser):
     name = models.CharField(max_length=255)
     email = models.EmailField()
     password = models.CharField(max_length=255)
+    
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='persons',
+        blank=True,
+        help_text='The groups this person belongs to.',
+        verbose_name='groups',
+    )
+    
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='persons',
+        blank=True,
+        help_text='Specific permissions for this person.',
+        verbose_name='user permissions',
+    )

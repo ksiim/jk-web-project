841b7a51b52a37126a32dbcc101291e93ffc796f
ksiim <makskrutchev@gmail.com>
2024-04-14 22:04:04 +0700

    added some views))

--- digitolio/persons/views.py
+++ digitolio/persons/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Person
+from django.http import HttpResponseNotFound
 
 # Create your views here.
+def index(request):
+    users = Person.objects.select_related("projects").all()
+    return render(request, 'users.html', context={'users': users})
+def register_person(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        person = Person.objects.create(
+            name=name,
+            email=email,
+            password=password
+        )
+        return render(request, 'person.html', context={'person': person})
+    return render(request, 'register_person.html')
--- digitolio/projects/views.py
+++ digitolio/projects/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from .models import Project
 
 def index(request):
-    return render(request, 'users.html')
+    projects = Project.objects.all()
+    return render(request, 'users.html', context={'projects': projects})
+
+def create_project(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        banner = request.POST.get('banner')
+        hashtags = request.POST.get('hashtags')
+        link_on_code = request.POST.get('link_on_code')
+        project = Project.objects.create(
+            title=title,
+            description=description,
+            banner=banner,
+            hashtags=hashtags,
+            link_on_code=link_on_code
+        )
+        return render(request, 'project.html', context={'project': project})
+    return render(request, 'create_project.html')

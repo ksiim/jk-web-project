2f51d78213d3d8bf7344a11f5b21751f2eac5b4b
LicoriceAlex <aleksandrsavin03@mail.ru>
2024-06-16 16:59:18 +0500

    bug fix

--- digitolio/persons/urls.py
+++ digitolio/persons/urls.py
@@ -1,22 +1,15 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
 from . import views
+from .forms import CustomUserAuthenticationForm
 
 app_name = 'persons'
 
 urlpatterns = [
     path('update/<str:username>', views.profile_update_view, name='profile_update'),
     path('logout/', views.person_logout, name='logout'),
-    path('login/', LoginView.as_view(template_name='persons/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='persons/login.html', form_class=CustomUserAuthenticationForm), name='login'),
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('', views.persons, name='persons'),
     path('<str:username>/', views.profile_view, name='profile_view'),
-    
-
-    # path('password_change/', PasswordChangeView.as_view(template_name='password_change_form.html'), name='password_change'),
-    # path('password_change/done/', PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
-    # path('password_reset/', PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
-    # path('password_reset/done/', PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
-    # path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
-    # path('reset/done/', PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
